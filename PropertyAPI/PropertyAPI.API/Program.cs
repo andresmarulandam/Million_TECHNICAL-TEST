3155b09aa4c2aa4f4cc87f2706eb642e261d0da1
Andres M <marulandaandres19@gmail.com>
2025-09-11 15:42:26 -0500

    feat: Add CORS policy to allow requests from React app origins

--- PropertyAPI/PropertyAPI.API/Program.cs
+++ PropertyAPI/PropertyAPI.API/Program.cs
@@ -8,6 +8,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Add layers
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
