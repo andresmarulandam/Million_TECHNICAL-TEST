837f4c3ce726dbc76ba6a06e5d917c459d956f1b
Andres M <marulandaandres19@gmail.com>
2025-09-11 10:06:58 -0500

    Setup for testing

---
+++ PropertyAPI.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PropertyAPI.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
